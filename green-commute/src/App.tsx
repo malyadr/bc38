66b01c5c8401a13aa3ddd69bb0e21b223a814a68
sow-mya-27 <sowmya.gadireddy@zemosolabs.com>
2022-05-24 10:38:25 +0530

    changes

--- green-commute/src/App.tsx
+++ green-commute/src/App.tsx
@@ -1,16 +1,7 @@
-import { Typography } from "@mui/material";
 import React from "react";
-import { Button1 } from "./components/molecules/button";
-// import { Chip } from "./components/molecules/chips";
 
 function App() {
-  return (
-    <div>
-      {/* <Chip text="Hyderabad" color="gammaColor1"></Chip> */}
-      <Button1>Next</Button1>
-      <Typography variant="h1">hi</Typography>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
