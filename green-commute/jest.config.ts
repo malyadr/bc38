a161299dd939f7518c0d07b9c7929b3b1957e849
sow-mya-27 <sowmya.gadireddy@zemosolabs.com>
2022-05-25 12:58:30 +0530

    changes in project structure

--- green-commute/jest.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    transform: {
-      ".+\\.(css|scss|png|jpg|svg|webp)$": "jest-transform-stub",
-    },
-    preset: "ts-jest",
-    testEnvironment: "jsdom",
-};
---
+++ src/components/molecules/button/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Button } from '@mui/material'
+import { Typography } from '@mui/material'
+import { textVariant, color, textColor } from '../../../theme/customTheme'
+
+interface ButtonProps {
+    variant: textVariant
+    TextColor: textColor
+    buttonVariant: 'text' | 'contained' | 'outlined' | undefined
+    buttonColor: color
+    children: React.ReactNode
+    styles: React.CSSProperties | null
+    onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined
+}
+
+export const Button1 = ({
+    variant,
+    TextColor,
+    buttonVariant,
+    buttonColor,
+    children,
+    styles,
+    onClick,
+}: ButtonProps) => {
+    return (
+        <>
+            <Button
+                sx={styles}
+                variant={buttonVariant}
+                color={buttonColor}
+                onClick={onClick}
+                style={{ borderRadius: '8px' }}
+            >
+                <Typography variant={variant} color={TextColor}>
+                    {children}
+                </Typography>
+            </Button>
+        </>
+    )
+}
