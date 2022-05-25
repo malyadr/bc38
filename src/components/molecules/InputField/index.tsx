e1ef5a539c02a70bbeda3220f46aebe2c02ee1dc
sow-mya-27 <sowmya.gadireddy@zemosolabs.com>
2022-05-25 12:58:30 +0530

    changes in project structure

---
+++ src/components/molecules/InputField/index.tsx
@@ -0,0 +1,12 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+interface input {
+    text: string
+    variant: 'standard' | 'outlined' | 'filled' | undefined
+    styles: React.CSSProperties | null
+}
+
+export const InputField = ({ text, variant, styles }: input) => {
+    return <TextField variant={variant} sx={styles} placeholder={text} />
+}
