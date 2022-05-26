d39aa4cc6189c6e387c0fb3d21f17f95634d58f0
MdTabraiz <tabraiz.md@zemosolabs.com>
2022-05-26 12:20:19 +0530

    icon fix

--- src/components/atoms/icon/index.test.tsx
+++ src/components/atoms/icon/index.test.tsx
@@ -1,11 +1,10 @@
-import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from "@testing-library/react";
-import Icon from "./index";
-import save from "../../../assets/icons/save.svg";
-import React from "react";
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Icon from './index'
+import save from '../../../../public/assets/icons/save.svg'
+import React from 'react'
 
-it("renders mui icons", () => {
-  render(<Icon src={save} />);
-  const testIcon = screen.getByTestId("icon");
-  
-});
+it('renders mui icons', () => {
+    render(<Icon src={save} />)
+    const testIcon = screen.getByTestId('icon')
+})
