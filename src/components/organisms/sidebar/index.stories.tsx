52d3a5553924efa111324fc1cd02b9e8e9e4d5c7
Himanshu199328 <himanshu@zemosolabs.com>
2022-06-01 10:29:18 +0530

    sidebar

---
+++ src/components/organisms/sidebar/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import SideNav from '.';
+
+export default {
+  title: 'Organisms/SideNav',
+  component: SideNav,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/aAZmibHibDJ541oKCpxVso/Green-Commute?node-id=78%3A28901',
+    },
+  },
+} as ComponentMeta<typeof SideNav>;
+
+const Template: ComponentStory<typeof SideNav> = (args) => (
+  <SideNav />
+);
+
+export const Card = Template.bind({});
