60eb0b8bce8922664ea5dcc5b8d160ab1dfa092d
Sai-Dharaneesh264 <saidharaneesh.gurram@zemosolabs.com>
2022-06-07 01:20:29 +0530

    added HomePage Template

---
+++ src/components/templates/HomePageTemplate/index.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import HomePageTemplate from '.';
+
+export default {
+  title: 'Template/HomePage',
+  component: HomePageTemplate,
+} as ComponentMeta<typeof HomePageTemplate>;
+
+const Template: ComponentStory<typeof HomePageTemplate> = (args) => (
+  <HomePageTemplate />
+);
+
+export const Card = Template.bind({});
---
+++ src/components/templates/HomePageTemplate/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../organisms/header'
+import SideNav from '../../organisms/sidebar'
+
+const HomePageTemplate = () => {
+  return (
+    <>
+    <Header />
+    <SideNav />
+    </>
+  )
+}
+
+export default HomePageTemplate
