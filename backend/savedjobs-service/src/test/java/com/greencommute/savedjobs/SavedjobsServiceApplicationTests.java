6efa68fbc7bb08d947b7688295b3c907facce7b6
Sai-Dharaneesh264 <saidharaneesh.gurram@zemosolabs.com>
2022-06-30 18:33:44 +0530

    backend scaffolding

---
+++ backend/savedjobs-service/src/test/java/com/greencommute/savedjobs/SavedjobsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greencommute.savedjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavedjobsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
