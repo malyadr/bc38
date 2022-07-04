21b707f633bf24f94fb0917ffae2c78042e522d6
Sai Dharaneesh Gurram <saidharaneesh.gurram@zemosolabs.com>
2022-07-04 16:05:52 +0530

    Spring/sonar integration

--- backend/routes-service/src/test/java/com/greencommute/routes/RoutesServiceApplicationTests.java
+++ backend/routes-service/src/test/java/com/greencommute/routes/RoutesServiceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RoutesServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
--- backend/savedjobs-service/src/test/java/com/greencommute/savedjobs/SavedjobsServiceApplicationTests.java
+++ backend/savedjobs-service/src/test/java/com/greencommute/savedjobs/SavedjobsServiceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SavedjobsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
