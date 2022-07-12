6ceeed7484749298fcfd91a91937d679a869f54e
sow-mya-27 <sowmya.gadireddy@zemosolabs.com>
2022-07-12 16:19:44 +0530

    edited sql file

--- database/db/migrations/V1_insert_route.sql
+++ database/db/migrations/V1_insert_route.sql
@@ -5,6 +5,6 @@ INSERT INTO `green_commute_db`.`routes` (`bus`, `cab`, `metro`, `bike`, `job_id`
 INSERT INTO `green_commute_db`.`routes` (`bus`, `cab`, `metro`, `bike`, `job_id`, `id`) VALUES (1, 1, 1, 0, 3, 3);
 INSERT INTO `green_commute_db`.`routes` (`bus`, `cab`, `metro`, `bike`, `job_id`, `id`) VALUES (1, 1, 1, 0, 4, 4);
 INSERT INTO `green_commute_db`.`routes` (`bus`, `cab`, `metro`, `bike`, `job_id`, `id`) VALUES (0, 1, 1, 1, 5, 5);
-INSERT INTO `green_commute_db`.`routes` (`bus`, `cab`, `metro`, `bike`, `job_id`, `id`) VALUES (0, 1, 1, 1, 6, 5);
+INSERT INTO `green_commute_db`.`routes` (`bus`, `cab`, `metro`, `bike`, `job_id`, `id`) VALUES (0, 1, 1, 1, 6, 6);
 
 COMMIT;
