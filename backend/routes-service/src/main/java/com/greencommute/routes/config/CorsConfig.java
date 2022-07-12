8347ccb490f7c20414236bc5ce74d9ff86de734b
Sai-Dharaneesh264 <saidharaneesh.gurram@zemosolabs.com>
2022-07-12 19:00:52 +0530

    resolved cors errors

--- backend/routes-service/src/main/java/com/greencommute/routes/config/CorsConfig.java
+++ backend/routes-service/src/main/java/com/greencommute/routes/config/CorsConfig.java
@@ -1,10 +1,14 @@
 package com.greencommute.routes.config;
 
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
--- backend/skills-service/src/main/java/com/greencommute/skills/config/CorsConfig.java
+++ backend/skills-service/src/main/java/com/greencommute/skills/config/CorsConfig.java
@@ -1,10 +1,13 @@
 package com.greencommute.skills.config;
 
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
