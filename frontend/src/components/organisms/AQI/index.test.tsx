bda150966fdc82eb181381c2ffb3c3f4d7d5c0ac
sow-mya-27 <sowmya.gadireddy@zemosolabs.com>
2022-07-13 14:03:52 +0530

    coverage

--- frontend/src/components/organisms/AQI/index.test.tsx
+++ frontend/src/components/organisms/AQI/index.test.tsx
@@ -11,9 +11,28 @@ const defaultValue = {
             name: 'hyderabad',
             aqi: 894,
         },
+        {
+            name: 'mumbai',
+            aqi: 200,
+        },
+        {
+            name: 'secunderabad',
+            aqi: 500,
+        },
     ],
     mySkills: {
         skills: ['ux'],
+        numberOfJobs: 5,
+    },
+}
+const empty = {
+    currentLocation: {
+        name: '',
+        aqi: 894,
+    },
+    jobLocation: [],
+    mySkills: {
+        skills: [],
         numberOfJobs: 5,
     },
 }
@@ -26,4 +45,15 @@ it('render step 0', () => {
 })
 it('render step 1', () => {
     render(<AQI details={defaultValue} step={1}></AQI>)
+})
+
+it('render step 0 with empty', () => {
+    render(<AQI details={empty} step={0}></AQI>)
+})
+it('render step 1 with empty', () => {
+    render(<AQI details={empty} step={1}></AQI>)
+})
+
+it('render step 2 with empty', () => {
+    render(<AQI details={empty} step={2}></AQI>)
 })
